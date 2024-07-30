37eaa2250929b03b566732f016fab1cbd74580f5
“David <dulucdavid1@gmail.com>
2024-07-30 08:48:32 -0400

    fix: update project fixtures and views to use placeholder images
    - Update project fixtures to use placeholder image URLs
    - Modify project views to handle cases where image might not be present
    - Adjust tests to reflect changes in project data
    
    This commit resolves issues with missing image assets in tests and
    ensures consistent display of project images across the application.

--- test/controllers/blog_posts_controller_test.rb
+++ test/controllers/blog_posts_controller_test.rb
@@ -2,12 +2,13 @@ require "test_helper"
 
 class BlogPostsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get blog_posts_index_url
+    get blog_posts_url
     assert_response :success
   end
 
   test "should get show" do
-    get blog_posts_show_url
+    blog_post = blog_posts(:one)
+    get blog_post_url(blog_post)
     assert_response :success
   end
 end
